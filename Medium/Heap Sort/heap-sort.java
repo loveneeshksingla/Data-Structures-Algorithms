2f143e78df5ed13b40ad8d7f4e150f4993126ec5
Loveneesh Singla <102587307+loveneeshksingla@users.noreply.github.com>
2024-03-16 19:41:58 +0530

    Updated Solution - GfG to GitHub

--- Medium/Heap Sort/heap-sort.java
+++ Medium/Heap Sort/heap-sort.java
@@ -35,55 +35,58 @@ class Heap_Sort
 // } Driver Code Ends
 
 
-
-
 class Solution
 {
     //Function to build a Heap from array.
     void buildHeap(int arr[], int n)
     {
-        for(int i = (n/2-1); i >= 0; i--) {
-            heapify(arr, n, i);
+        // Your code here
+        for(int ind = (n/2-1); ind >= 0; ind--) {
+            heapify(arr, n, ind);
         }
+        
     }
  
     //Heapify function to maintain heap property.
     void heapify(int arr[], int n, int i)
     {
-        int left = 2*i+1;
-        int right = 2*i+2;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
         
         int largest = i;
         
-        if(left < n && arr[left] > arr[largest]) {
+        if(left < n && arr[largest] < arr[left]){
             largest = left;
         }
         
-        if(right < n && arr[right] > arr[largest]) {
+        if(right < n && arr[largest] < arr[right]) {
             largest = right;
         }
         
         
         if(largest != i) {
-            swap(arr, i, largest);
+            swap(arr, largest, i);
             heapify(arr, n, largest);
         }
         
+        
+        
     }
     
-    public void swap(int[] arr, int x, int y) {
-        int temp = arr[x];
-        arr[x] = arr[y];
-        arr[y] = temp;
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
     
     //Function to sort an array using Heap Sort.
     public void heapSort(int arr[], int n)
     {
+        
         buildHeap(arr, n);
         
-        for(int i = n-1; i >= 0; i--) {
-            swap(arr, 0, i);
+        for(int i = n-1; i > 0; i--) {
+            swap(arr, i, 0);
             heapify(arr, i, 0);
         }
     }
