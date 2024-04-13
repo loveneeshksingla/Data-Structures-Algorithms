38e50906daf49b9b89b26f6be0196a348dcb7ff7
Loveneesh Singla <102587307+loveneeshksingla@users.noreply.github.com>
2024-04-13 19:33:53 +0530

    Updated Solution - GfG to GitHub

--- Medium/Maximum Index/maximum-index.java
+++ Medium/Maximum Index/maximum-index.java
@@ -2,38 +2,32 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-    
-	public static void main (String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
-		while(t-->0){
-		    int n = Integer.parseInt(br.readLine().trim()); // size of array
-		    int arr[] = new int[n];
-		    String inputLine[] = br.readLine().trim().split(" ");
-		    for(int i=0; i<n; i++){
-		        arr[i] = Integer.parseInt(inputLine[i]); // input elements of array
-		    }
-		    
-		    Solution ob = new Solution();
-		    
-		    System.out.println(ob.maxIndexDiff(arr, n)); // print the result
-		}
-	}
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            System.out.println(new Solution().maxIndexDiff(arr, n));
+        }
+    }
 }
 // } Driver Code Ends
 
 
+class Solution {
 
-
-class Solution{
-    
-    // A[]: input array
-    // N: size of array
-    // Function to find the maximum index difference.
-    static int maxIndexDiff(int A[], int N) { 
+    int maxIndexDiff(int A[], int n) {
+        // code here
         
-        // Your code here
+        
         
         
         int[] leftMin = new int[N];
@@ -69,7 +63,6 @@ class Solution{
         }
         
         return max;
-        
         
     }
 }
