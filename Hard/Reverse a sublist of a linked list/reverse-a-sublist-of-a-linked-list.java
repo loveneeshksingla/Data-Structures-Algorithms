a877fe9172c87106d11af77d4a09f68ed7182833
Loveneesh Singla <102587307+loveneeshksingla@users.noreply.github.com>
2024-06-07 22:11:06 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Reverse a sublist of a linked list/reverse-a-sublist-of-a-linked-list.java
@@ -0,0 +1,127 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class insertion
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	/* Drier program to test above functions */
+	public static void main(String args[])throws IOException
+    {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // Scanner sc = new Scanner(System.in);
+		 int t=Integer.parseInt(br.readLine());
+		 while(t>0)
+         {
+            String S[] = br.readLine().split(" ");
+			int N = Integer.parseInt(S[0]);
+            int m = Integer.parseInt(S[1]);
+            int n = Integer.parseInt(S[2]);
+		
+		    String S1[] = br.readLine().split(" ");
+			insertion llist = new insertion(); 
+			int a1=Integer.parseInt(S1[0]);
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < N; i++) 
+			{
+				int a = Integer.parseInt(S1[i]);
+				llist.addToTheLast(new Node(a));
+			}
+			
+        Solution ob = new Solution();
+		Node newhead=ob.reverseBetween(llist.head, m, n);
+		llist.printList(newhead);
+		
+        t--;		
+        }
+    }}
+// } Driver Code Ends
+
+
+
+
+//User function Template for Java
+
+/*
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+    */
+class Solution
+{
+    public static Node reverseBetween(Node head, int m, int n)
+    {
+        //code here
+        
+        Node tempHead = head;
+        int count  = 1;
+        
+        Node dummyNode = new Node(0);
+        Node prev = dummyNode;
+        
+        while(count < m && tempHead != null) {
+            prev.next = tempHead;
+            prev = prev.next;
+            tempHead = tempHead.next;
+            count++;
+        }
+        
+        Node next = null;
+        Node tail = tempHead;
+        
+        while(count <= n && tempHead != null) {
+            Node temp = tempHead;
+            tempHead = tempHead.next;
+            temp.next = next;
+            next = temp;
+            count++;
+        }
+        
+        prev.next = next;
+        tail.next = tempHead;
+        
+        return dummyNode.next;
+        
+    }
+}
+
+
+
+
+
