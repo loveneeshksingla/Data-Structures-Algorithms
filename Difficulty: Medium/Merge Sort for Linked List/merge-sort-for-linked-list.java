81a00b97ab656a371b47b728c49fa50e51e7fc34
Loveneesh Singla <102587307+loveneeshksingla@users.noreply.github.com>
2024-07-29 10:24:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge Sort for Linked List/merge-sort-for-linked-list.java
@@ -0,0 +1,136 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        this.data = key;
+        next = null;
+    }
+} */
+
+class Solution
+{
+    //Function to sort the given linked list using Merge Sort.
+    static Node mergeSort(Node head)
+    {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        Node mid = getMid(head);
+        Node left = mergeSort(head);
+        Node right = mergeSort(mid);
+        return merge(left, right);
+        
+    }
+    
+    static Node merge(Node left, Node right) {
+        
+        Node dummy = new Node(0);
+        Node prev = dummy;
+        
+        while(left != null && right != null) {
+            if(left.data < right.data) {
+                prev.next = left;
+                left = left.next;
+                prev = prev.next;
+                prev.next = null;
+            }else {
+                prev.next = right;
+                
+                right = right.next;
+                prev = prev.next;
+                prev.next = null;
+            }
+        }
+        
+        if(left != null) prev.next = left;
+        if(right != null) prev.next = right;
+        
+        return dummy.next;
+        
+    }
+    
+    
+    
+    static Node getMid(Node head) {
+        
+        Node slow = head;
+        Node fast = head;
+        Node prev = null;
+        while(fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        if(prev != null) prev.next  = null;
+        return slow;
+        
+    }
+    
+    
+}
+
+
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            List<Integer> arr = new ArrayList<>();
+            String input = br.readLine().trim();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+
+            Node head = null;
+            if (!arr.isEmpty()) {
+                head = new Node(arr.get(0));
+                Node tail = head;
+                for (int i = 1; i < arr.size(); i++) {
+                    tail.next = new Node(arr.get(i));
+                    tail = tail.next;
+                }
+            }
+            head = new Solution().mergeSort(head);
+            printList(head);
+        }
+    }
+}
+
+// } Driver Code Ends
