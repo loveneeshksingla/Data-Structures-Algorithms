b54c9eac3ff8c227238efe9a21a6e6674ad1155b
Loveneesh Singla <102587307+loveneeshksingla@users.noreply.github.com>
2024-08-16 22:56:45 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.java
@@ -0,0 +1,168 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int d) {
+        data = d;
+        left = right = null;
+    }
+}
+
+class GFG {
+    static Node buildTree(String str) {
+        // Corner Case
+        if (str.length() == 0 || str.equals('N')) return null;
+        String[] s = str.split(" ");
+
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+
+        // Starting from the second element
+        int i = 1;
+        while (!q.isEmpty() && i < s.length) {
+            // Get and remove the front of the queue
+            Node currNode = q.remove();
+
+            // Get the curr node's value from the string
+            String currVal = s[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the curr node
+                currNode.left = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= s.length) break;
+            currVal = s[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the curr node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.right);
+            }
+
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t > 0) {
+            String s = br.readLine();
+            Node root1 = buildTree(s);
+
+            s = br.readLine();
+            Node root2 = buildTree(s);
+
+            Solution T = new Solution();
+            List<Integer> ans = T.merge(root1, root2);
+            for (int i = 0; i < ans.size(); i++) System.out.print(ans.get(i) + " ");
+            System.out.println();
+
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+
+
+//User function Template for Java
+
+
+/*
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+    
+*/
+class Solution
+{
+    //Function to return a list of integers denoting the node 
+    //values of both the BST in a sorted order.
+    public List<Integer> merge(Node root1,Node root2)
+    {
+        // Write your code here
+        
+        Stack<Node> s1 = new Stack<>();
+        Stack<Node> s2 = new Stack<>();
+        
+        List<Integer> ans = new ArrayList<>();
+        
+        while(!s1.isEmpty() || !s2.isEmpty() || root1 != null || root2 != null) {
+            
+            while(root1 != null) {
+                s1.push(root1);
+                root1 = root1.left;
+            }
+            
+            while(root2 != null) {
+                s2.push(root2);
+                root2 = root2.left;
+            }
+            
+            if(s2.isEmpty() || (!s1.isEmpty() && s1.peek().data <= s2.peek().data)) {
+                Node cur = s1.pop();
+                ans.add(cur.data);
+                root1 = cur.right;
+            }else {
+                Node cur = s2.pop();
+                ans.add(cur.data);
+                root2 = cur.right;
+            }
+        }
+        
+        
+        return ans;
+        
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
