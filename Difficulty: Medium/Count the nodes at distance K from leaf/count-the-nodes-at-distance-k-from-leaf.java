229f4d1e4717f46e89e69b58a8299fa0b2a11f29
Loveneesh Singla <102587307+loveneeshksingla@users.noreply.github.com>
2024-08-17 08:21:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count the nodes at distance K from leaf/count-the-nodes-at-distance-k-from-leaf.java
@@ -0,0 +1,164 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+
+class GFG
+{
+    static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length() == 0 || str.equals('N'))
+            return null;
+        String[] s = str.split(" ");
+        
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue <Node> q = new LinkedList<Node>();
+        q.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(!q.isEmpty() && i < s.length)
+        {
+              // Get and remove the front of the queue
+              Node currrNode = q.remove();
+        
+              // Get the currrent node's value from the string
+              String currrVal = s[i];
+        
+              // If the left child is not null
+              if(!currrVal.equals("N")) 
+              {
+        
+                  // Create the left child for the currrent node
+                  currrNode.left = new Node(Integer.parseInt(currrVal));
+        
+                  // Push it to the queue
+                  q.add(currrNode.left);
+              }
+        
+              // For the right child
+              i++;
+              if(i >= s.length)
+                  break;
+              currrVal = s[i];
+        
+              // If the right child is not null
+              if(!currrVal.equals("N")) 
+              {
+        
+                  // Create the right child for the currrent node
+                  currrNode.right = new Node(Integer.parseInt(currrVal));
+        
+                  // Push it to the queue
+                  q.add(currrNode.right);
+              }
+              
+              i++;
+        }
+    
+        return root;
+    }
+    
+    public static void main(String args[]) throws IOException {
+    
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t>0)
+        {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            
+            int k = Integer.parseInt(br.readLine().trim());
+            
+            Solution T = new Solution();
+            System.out.println(T.printKDistantfromLeaf(root,k));
+            t--;
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+
+
+//User function Template for Java
+
+// class Node  
+// { 
+//     int data; 
+//     Node left, right; 
+   
+//     public Node(int d)  
+//     { 
+//         data = d; 
+//         left = right = null; 
+//     } 
+// }
+
+class Solution
+{
+    
+    //Function to return count of nodes at a given distance from leaf nodes.
+    int printKDistantfromLeaf(Node root, int k)
+    {
+        // Write your code here
+        int[] count = new int[1];
+        boolean[] visited = new boolean[100000];
+        solve(root, k, count, 0, visited);
+        
+        return count[0];
+    }
+    
+    void solve(Node root, int k, int[] count, int curLevel, boolean[] visited) {
+        
+        if(root == null) {
+            return ;
+        }
+        
+        visited[curLevel] = false;
+        
+        if(isLeaf(root)) {
+            if(curLevel - k >= 0) {
+                if(!visited[curLevel - k]) {
+                    count[0]++;
+                    visited[curLevel - k] = true;
+                }
+            }
+            return;
+        }
+        
+        solve(root.left, k, count, curLevel+1, visited);
+        solve(root.right, k, count, curLevel+1, visited);
+        
+    }
+    
+    boolean isLeaf(Node root) {
+        return root.left == null && root.right == null;
+    }
+
+}
+
+
+
+
