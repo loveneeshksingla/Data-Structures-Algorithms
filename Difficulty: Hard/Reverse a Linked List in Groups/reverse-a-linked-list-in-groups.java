8bdcd5d899f9cfea23992cad4d0b02ad29bf6078
Loveneesh Singla <102587307+loveneeshksingla@users.noreply.github.com>
2024-09-26 23:12:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Reverse a Linked List in Groups/reverse-a-linked-list-in-groups.java
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int key) {
+        data = key;
+        next = null;
+    }
+}
+
+class ReverseInSize {
+    static Node head;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+
+        while (t-- > 0) {
+
+            String s[] = in.readLine().trim().split(" ");
+            int a1 = Integer.parseInt(s[0]);
+            Node head = new Node(a1);
+            Node tail = head;
+            for (int i = 1; i < s.length; i++) {
+                int a = Integer.parseInt(s[i]);
+                // addToTheLast(new Node(a));
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+
+            int k = Integer.parseInt(in.readLine().trim());
+            Solution ob = new Solution();
+            Node res = ob.reverse(head, k);
+            printList(res, out);
+            out.println();
+        }
+        out.close();
+    }
+
+    public static void printList(Node node, PrintWriter out) {
+        while (node != null) {
+            out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+/*node class of the linked list
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class Solution
+{
+    public static Node reverse(Node node, int k)
+    {
+        //Your code here
+        
+        Node dummy = new Node(0);
+        Node tail = dummy;
+        // Node prev = dummy;
+        
+        while(node != null) {
+            int count = 1;
+            Node prev = null;
+            while(node != null && count <= k) {
+                Node temp = node;
+                node = node.next;
+                temp.next = prev;
+                prev = temp;
+                count++;
+            }
+            while(tail.next != null) {
+                tail = tail.next;
+            }
+            tail.next = prev;
+        }
+        
+        
+        return dummy.next;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
