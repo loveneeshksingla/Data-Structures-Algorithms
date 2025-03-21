92de50457136b4ee9a489ca09343ffcefa290e04
Loveneesh Singla <102587307+loveneeshksingla@users.noreply.github.com>
2025-03-22 00:16:25 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Greater Element/next-greater-element.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        for (int g = 0; g < t; g++) {
+            String[] str =
+                (br.readLine()).trim().split(" "); // Reading input as a string array
+            int arr[] = new int[str.length]; // Creating integer array from the input
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            // Getting the result from the Solution class
+            ArrayList<Integer> result = new Solution().nextLargerElement(arr);
+
+            // Printing the result in the required format
+            if (result.isEmpty()) {
+                System.out.println("[]");
+            } else {
+                for (int i = 0; i < result.size(); i++) {
+                    if (i != 0) System.out.print(" ");
+                    System.out.print(result.get(i));
+                }
+                System.out.println();
+                System.out.println("~");
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find the next greater element for each element of the array.
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        // code here
+        
+        
+        Stack<Integer> sta = new Stack<>();
+        int arrLen = arr.length;
+        int[] answer = new int[arrLen];
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        for(int ind = arrLen-1; ind >= 0; ind--) {
+            if(sta.isEmpty()) {
+                answer[ind] = -1;
+            } else {
+                while(!sta.isEmpty() && sta.peek() <= arr[ind]) {
+                    sta.pop();
+                }
+                if (sta.isEmpty()) {
+                    answer[ind] = -1;
+                } else {
+                    answer[ind] = sta.peek();
+                }
+            }
+            sta.push(arr[ind]);
+        }
+        
+        for (int ind = 0; ind < arrLen; ind++) {
+            ans.add(answer[ind]);
+        }
+        
+        return ans;
+    }
+}
+
+
+
