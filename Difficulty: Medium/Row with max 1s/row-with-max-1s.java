681590c06f023bf2b9c00634d79e5fd646801720
Loveneesh Singla <102587307+loveneeshksingla@users.noreply.github.com>
2025-04-02 02:16:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.java
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.java
@@ -1,27 +1,51 @@
 //{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt();
-        while (tc-- > 0) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            int arr[][] = new int[n][m];
+        int t = sc.nextInt(); // Number of test cases
+        sc.nextLine();        // Consume the newline character
 
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    arr[i][j] = sc.nextInt();
+        while (t-- > 0) {
+            String input = sc.nextLine();
+
+            // Replace ][ with ],[
+            input = input.replace("][", "],[");
+
+            ArrayList<ArrayList<Integer>> mat = new ArrayList<>();
+            String[] rows = input.split("],\\s*\\[");
+
+            for (String row : rows) {
+                row = row.replaceAll("[\\[\\]]", ""); // Remove any surrounding brackets
+                ArrayList<Integer> intRow = new ArrayList<>();
+                for (String num : row.split(",")) {
+                    intRow.add(Integer.parseInt(num.trim()));
                 }
+                mat.add(intRow);
             }
-            int ans = new Solution().rowWithMax1s(arr);
-            System.out.println(ans);
+
+            Solution obj = new Solution();
+            int result = obj.rowWithMax1s(convertListToArray(mat));
+
+            System.out.println(result);
+
+            System.out.println("~");
         }
+        sc.close();
+    }
+
+    // Helper method to convert ArrayList<ArrayList<Integer>> to 2D array
+    public static int[][] convertListToArray(ArrayList<ArrayList<Integer>> mat) {
+        int rows = mat.size();
+        int cols = mat.get(0).size();
+        int[][] arr = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = mat.get(i).get(j);
+            }
+        }
+        return arr;
     }
 }
 
@@ -34,49 +58,49 @@ class Solution {
     public int rowWithMax1s(int arr[][]) {
         // code here
         
-        int rows = arr.length;
-        int cols = arr[0].length;
         
-        int max = Integer.MIN_VALUE;
-        int row = -1;
+        int numRows = arr.length;
+        int numCols = arr[0].length;
         
-        for ( int rowInd = 0; rowInd < rows; rowInd++ ) {
+        int rowWithMaxOnes = -1;
+        int maxOne = 0;
+        
+        for (int rowInd = 0; rowInd < numRows; rowInd++) {
             
-            int firstInd = getOnesFirtsInd(arr[rowInd]);
-            
-            // System.out.println(firstInd + "m");
-            
-            if ( firstInd < cols ) {
-                int length = cols - firstInd;
-            
-                if( max < length) {
-                    max = length;
-                    row = rowInd;
-                }
+            int onesFirstInd = binarySearch(arr[rowInd]);
+            int curMaxOnes = numCols - onesFirstInd;
+            if (maxOne < curMaxOnes) {
+                maxOne = curMaxOnes;
+                rowWithMaxOnes = rowInd;
             }
+            
         }
         
-        return row;
+        return rowWithMaxOnes;
     }
     
-    
-    public int getOnesFirtsInd ( int[] arr) {
+    public int binarySearch(int[] arr) {
         
-        int left = 0;
-        int right = arr.length-1;
-    
+        int numCols = arr.length;
         
-        while ( left <= right ) {
-            
-            int mid = (left + right) / 2;
+        int leftInd = 0;
+        int rightInd = numCols-1;
+        
+        while (leftInd <= rightInd) {
+            int mid = (leftInd + rightInd) / 2;
             
             if (arr[mid] == 1) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+                rightInd--;
+            }else {
+                leftInd++;
             }
         }
         
-        return left;
+        return leftInd;
+        
     }
+    
+    
 }
+
+
