4f18ad98eff8d846e9eee6530e6b299f6e478907
Loveneesh Singla <102587307+loveneeshksingla@users.noreply.github.com>
2025-04-02 23:43:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Overlapping Intervals/overlapping-intervals.java
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.java
@@ -1,31 +1,27 @@
 //{ Driver Code Starts
-import java.util.*;
-import java.lang.*;
 import java.io.*;
-class GFG
-{
-    public static void main(String[] args) throws IOException
-    {
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine().trim());
-        while(T-->0)
-        {
+        while (T-- > 0) {
             int n = Integer.parseInt(br.readLine().trim());
-            String[] s = br.readLine().trim().split(" ");
-            int[][] Intervals = new int[n][2];
-            int j = 0;
-            for(int i = 0; i < n; i++){
-                Intervals[i][0] = Integer.parseInt(s[j]);
-                j++;
-                Intervals[i][1] = Integer.parseInt(s[j]);
-                j++;
+            int[][] arr = new int[n][2];
+            for (int i = 0; i < n; i++) {
+                String temp[] = br.readLine().trim().split(" ");
+                arr[i][0] = Integer.parseInt(temp[0]);
+                String x = temp[1];
+                arr[i][1] = Integer.parseInt(x);
             }
             Solution obj = new Solution();
-            int[][] ans = obj.overlappedInterval(Intervals);
-            for(int i = 0; i < ans.length; i++){
-                for(j = 0; j < ans[i].length; j++){
-                    System.out.print(ans[i][j] + " ");
-                }
+            // The mergeOverlap function now returns a List<int[]>
+            List<int[]> ans = obj.mergeOverlap(arr);
+
+            // Printing the merged arr
+            for (int[] interval : ans) {
+                System.out.print(interval[0] + " " + interval[1] + " ");
             }
             System.out.println();
         }
@@ -35,61 +31,33 @@ class GFG
 // } Driver Code Ends
 
 
-
-
-class Solution
-{
-    public int[][] overlappedInterval(int[][] Intervals)
-    {
+class Solution {
+    public List<int[]> mergeOverlap(int[][] arr) {
         // Code here // Code here
         
-        Arrays.sort(Intervals, new Comp());
+        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+        int numRows = arr.length;
+        // for (int i = 0 ; i < numRows; i++) {
+        //     System.out.println(arr[i][0] + " " + arr[i][1]);
+        // }
+        List<int[]> ans = new ArrayList<>();
         
-        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
-        
-        ArrayList<Integer> temp = new ArrayList<>();
-        temp.add(Intervals[0][0]);
-        temp.add(Intervals[0][1]);
-        
-        ans.add(temp);
-        
-        int intervalsSize = Intervals.length;
-        
-        for(int ind = 1; ind < intervalsSize; ind++) {
-            temp = ans.get(ans.size()-1);
+        for (int rowInd = 0; rowInd < numRows-1; rowInd++) {
             
-            if(Intervals[ind][0] <= temp.get(1)) {
-                
-                int secondNum = Intervals[ind][1] > temp.get(1) ?  Intervals[ind][1] : temp.get(1);
-                
-                temp.set(1, secondNum);
-                
-                ans.set(ans.size()-1, temp);
+            int[] firstArr = arr[rowInd];
+            int[] secArr = arr[rowInd+1];
+            
+            if (firstArr[1] >= secArr[0]) {
+                arr[rowInd+1][1] = firstArr[1] > secArr[1] ? firstArr[1] : secArr[1];
+                arr[rowInd+1][0] = firstArr[0] < secArr[0] ? firstArr[0] : secArr[0];
             }else {
-                ArrayList<Integer> tempL = new ArrayList<>();
-                tempL.add(Intervals[ind][0]);
-                tempL.add(Intervals[ind][1]);
-                
-                ans.add(tempL);
+                ans.add(new int[] {firstArr[0], firstArr[1]});
             }
         }
-        
-        int[][] result = new int[ans.size()][2];
-        
-        for(int ind = 0; ind < ans.size(); ind++) {
-            ArrayList<Integer> curList = ans.get(ind);
-            result[ind][0] = curList.get(0);
-            result[ind][1] = curList.get(1);
-        }
-        
-        return result;
-        
-    }
-}
 
-class Comp implements Comparator<int[]> {
-    @Override
-    public int compare(int[] a, int[] b) {
-        return a[0] - b[0];
+        ans.add(new int[] {arr[numRows-1][0], arr[numRows-1][1]});
+      
+        
+        return ans;
     }
 }
