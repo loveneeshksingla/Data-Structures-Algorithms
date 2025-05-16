8426d8c8d38c348b2c3e4a8cc66977e0d1e0f1ab
Loveneesh Singla <102587307+loveneeshksingla@users.noreply.github.com>
2025-05-16 21:01:59 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum subset sum/maximum-subset-sum.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[] A = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            long res = obj.findMaxSubsetSum(N, A);
+            
+            System.out.println(res);
+            
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+
+    public static long findMaxSubsetSum(int N, int[] A) {
+        // code here
+        
+        
+        int[] dp = new int[N];
+        
+        dp[0] = A[0];
+        dp[1] = Math.max(A[1], dp[0] + A[1]);
+        
+        
+        for (int ind = 2; ind < N; ind++) {
+            
+            dp[ind] = Math.max((dp[ind-2]+A[ind]), dp[ind-1]+A[ind]);
+        }
+        
+        long max = Math.max(dp[N-1], dp[N-2]);
+        return max;
+    }
+}
