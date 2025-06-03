9695d1842950836a1b149b2fc2a81ed987ff320a
Loveneesh Singla <102587307+loveneeshksingla@users.noreply.github.com>
2025-06-03 20:38:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Nodes at given distance in binary tree/nodes-at-given-distance-in-binary-tree.java
+++ Difficulty: Hard/Nodes at given distance in binary tree/nodes-at-given-distance-in-binary-tree.java
@@ -1,71 +1,89 @@
-
-
-//User function Template for Java
-
-// class Node  
-// { 
-//     int data; 
+// class Node
+// {
+//     int data;
 //     Node left, right;
 // }
 
-class Solution
-{
+class Solution {
+    
+    
     static ArrayList<Integer> ans;
-    public static ArrayList<Integer> KDistanceNodes(Node root, int target , int k)
-    {
+    
+    public static ArrayList<Integer> KDistanceNodes(Node root, int target, int k) {
         // return the sorted list of all nodes at k dist
+        
         ans = new ArrayList<>();
-        solve(root, target, k);
+        solve(root, target, k, 0);
         Collections.sort(ans);
         return ans;
-        
     }
     
-    static int solve(Node root, int target, int k) {
+    
+    public static int solve(Node root, int target, int k, int curDis) {
         
-        if(root == null) return 0;
+        if (root == null) return 0;
         
-        if(root.data == target) {
-            getNodesAtDis(root, k, 0);
+        
+        if (root.data == target) {
+            findNodes(root, k, 0);
             return 1;
         }
         
-        int left = solve(root.left, target, k);
-        int right = solve(root.right, target, k);
+        int left = solve(root.left, target, k, curDis+1);
+        int right = solve(root.right, target, k, curDis+1);
         
         int depth = Math.max(left, right);
-        if(depth > 0) depth++;
+        if (depth > 0) depth++;
         
-        if(left > 0 && left <= k) {
-            if(left == k) 
+        if (left >= 1 && left <= k) {
+            
+            if (left == k) {
                 ans.add(root.data);
-            else
-                getNodesAtDis(root.right, k, left+1);
-        }
-        
-        if(right > 0 && right <= k) {
-            if(right == k) 
+               
+            } else {
+                findNodes(root.right, k, left+1);
+            }
+            
+            
+        } else if (right >= 1 && right <= k) {
+            
+            if (right == k) {
                 ans.add(root.data);
-            else 
-                getNodesAtDis(root.left, k, right+1);
+               
+            } else {
+                findNodes(root.left, k, right+1);
+            }
+            
         }
         
         return depth;
+        
+        
     }
     
-    static void getNodesAtDis(Node root, int k, int dis) {
+    
+    public static void findNodes(Node root, int k, int curDis) {
         
-        if(root == null || dis > k) return;
+        if (root == null) {
+            return;
+        }
         
-        if(dis == k) {
+        if (curDis == k) {
             ans.add(root.data);
             return;
         }
         
-        getNodesAtDis(root.left, k, dis+1);
-        getNodesAtDis(root.right, k, dis+1);
+        findNodes(root.left, k, curDis+1);
+        findNodes(root.right, k, curDis+1);
     }
+    
+    
+    
 };
+
+
+
+
 
 
 
